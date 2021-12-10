38db29e0622a8213a564021ff8e36f0a05b7f8a5
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-09 17:48:31 -0800

    added my info for pokemon

--- src/pokelist/Pokelist.js
+++ src/pokelist/Pokelist.js
@@ -1,8 +1,15 @@
 export default function PokeList({ pokemon }) {
   return (
-    <div>
+    <div className="pokedex">
       {pokemon.map((poke) => (
-        <p key={poke.id}>{poke.pokemon}</p>
+        <div key={poke.id} className="pokeCard">
+          <p key={poke.id}>{poke.pokemon}</p>
+          <img src={poke.url_image} style={{ height: '200px' }} />
+          <p>HP: {poke.hp}</p>
+          <p>Attack: {poke.attack}</p>
+          <p>Defense: {poke.defense}</p>
+          <p>Type: {poke.type_1}</p>
+        </div>
       ))}
     </div>
   );
