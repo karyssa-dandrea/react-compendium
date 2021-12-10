737e188a95b0f46a8b368885e62cb6f5982c735f
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-09 16:57:33 -0800

    changed my options for drop down to map

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,7 +5,6 @@ export default function Controls({
   setQuery,
   setLoading,
   types,
-  setTypes,
   selectedType,
   setSelectedType,
 }) {
@@ -20,24 +19,12 @@ export default function Controls({
         }}
       />
       <select value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
-        <option value="All">All</option>
-        <option value="electric">Electric</option>
-        <option value="fire">Fire</option>
-        <option value="ghost">Ghost</option>
-        <option value="fairy">Fairy</option>
-        <option value="grass">Grass</option>
-        <option value="ground">Ground</option>
-        <option value="flying">Flying</option>
-        <option value="ice">Ice</option>
-        <option value="fighting">Fighting</option>
-        <option value="dragon">Dragon</option>
-        <option value="poison">Poison</option>
-        <option value="steel">Steel</option>
-        <option value="dark">Dark</option>
-        <option value="psychic">Pyschic</option>
-        <option value="rock">Rock</option>
-        <option value="water">Water</option>
-        <option value="normal">Normal</option>
+        <option value="all">All</option>
+        {types.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
       </select>
       <Button variant="contained" size="small" onClick={() => setLoading(true)}>
         Search
