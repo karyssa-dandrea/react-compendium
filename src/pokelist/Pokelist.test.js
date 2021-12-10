504aac446a145ff5c2a41a9eb9de6d3beea22b22
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-10 12:13:38 -0800

    passed one test, and added css

--- src/pokelist/Pokelist.test.js
+++ src/pokelist/Pokelist.test.js
@@ -1,6 +1,7 @@
 import PokeList from './Pokelist';
+import { render } from '@testing-library/react';
 
 test('renders the pokemon names', () => {
   const pokeNames = render(<PokeList pokemon={['pikachu']} />);
-  expect(pokeNames).toMatchSnapshot;
+  expect(pokeNames).toMatchSnapshot();
 });
